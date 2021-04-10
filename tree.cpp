cd6eac68b144b33bcd709d5b8f12028bf70b6ff8
Raghu Vamshi <raghuvamshimailbox@gmail.com>
2021-04-10 15:45:17 -0400

    Added sample tree

--- tree.cpp
+++ tree.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main() {
   cout << "Tree Pattern Matching!!";
+  /* Tree Data */
+  string S[] = {"ab", "adc", "add", "ac"};
+  string T[] = {"ab", "acd", "acc", "ad"};
+  
+  /* Todo DTW */
+  
+  /* Todo LCS */
+  
+  
   return 0;
 }
