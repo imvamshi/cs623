9e19b6cec916cbbe00286acf9fe2fd213c874af0
Raghu Vamshi <raghu@Raghus-MacBook-Pro.local>
2021-04-13 13:03:25 -0400

    Created vector of strings

--- tree.cpp
+++ tree.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
   cout << "Tree Pattern Matching!!";
   /* Tree Data */
-  string S[] = {"ab", "adc", "add", "ac"};
-  string T[] = {"ab", "acd", "acc", "ad"};
+  vector <string> S = {"ab", "adc", "add", "ac"};
+  vector <string> T = {"ab", "acd", "acc", "ad"};
   
   /* Todo DTW */
   
   /* Todo LCS */
+  
+  /* Calculate dtwLcs */
+  int k, l;
+  k = l = 1;
+
   
   
   return 0;
