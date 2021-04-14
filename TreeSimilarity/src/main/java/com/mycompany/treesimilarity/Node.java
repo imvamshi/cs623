a7ffd033d5e89915be98714bea00f0af8d8783bf
AnCarro <41639595+AnCarro@users.noreply.github.com>
2021-04-14 19:38:05 -0400

    Java files for tree implementation
    These are files from Netbeans. I imagine they're openable from most Java IDEs. The tree still needs some work and I haven't really tested it yet. But the basics are there and it should function. I think the main thing left, is to perhaps implement an auto numbering for nodes in a tree, so that they're easily accessible.

---
+++ TreeSimilarity/src/main/java/com/mycompany/treesimilarity/Node.java
@@ -0,0 +1,214 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.treesimilarity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Towny
+ */
+public class Node {
+    private String Value;
+    private int Number = 0;
+    private List<Node> Children = new ArrayList<Node>();
+    private Node Parent;
+    
+    Node (String value) {
+        Value = value;
+        Parent = null;
+    }
+    
+    Node (int value) {
+        Value = String.valueOf(value);
+        Parent = null;
+    }    
+    
+    Node (String value, Node parent) {
+        Value = value;
+        Parent = parent;
+    }
+    
+    Node (int value, Node parent) {
+        Value = String.valueOf(value);
+        Parent = parent;
+    }
+    
+    Node (String value, int nodeNum) {
+        Value = value;
+        Parent = null;
+        Number = nodeNum;
+    }
+    
+    Node (int value, int nodeNum) {
+        Value = String.valueOf(value);
+        Parent = null;
+        Number = nodeNum;
+    }    
+    
+    Node (String value, Node parent, int nodeNum) {
+        Value = value;
+        Parent = parent;
+        Number = nodeNum;
+    }
+    
+    Node (int value, Node parent, int nodeNum) {
+        Value = String.valueOf(value);
+        Parent = parent;
+        Number = nodeNum;
+    }
+   /* 
+    Node (String value, Node parent, List<Node> children) {
+        Value = value;
+        addChildren(children);
+        Parent = parent;
+    }
+    
+    Node (int value, Node parent, List<Node> children) {
+        Value = String.valueOf(value);
+        Parent = parent;
+        addChildren(children);
+    }
+    
+    Node (String value, Node parent, String[] values) {
+        Value = value;
+        addChildren(values);
+        Parent = parent;
+    }
+    
+    Node (int value, Node parent, String[] values) {
+        Value = String.valueOf(value);
+        Parent = parent;
+        addChildren(values);
+    }
+    
+    
+    Node (String value, Node parent, Node child) {
+        Value = value;
+        addChild(child);
+        Parent = parent;
+    }
+    
+    Node (int value, Node parent, Node child) {
+        Value = String.valueOf(value);
+        Parent = parent;
+        addChild(child);
+    }
+    
+    Node (String value, Node parent, String child) {
+        Value = value;
+        addChild(child);
+        Parent = parent;
+    }
+    
+    Node (int value, Node parent, String child) {
+        Value = String.valueOf(value);
+        Parent = parent;
+        addChild(child);
+    }
+    */
+    
+    public boolean isLeaf(){
+        if(Children.isEmpty()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean isRoot() {
+        if(Parent == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public void setParent(Node node){
+       Parent = node;
+    }
+    
+    public void setValue(String string){
+        Value = string;
+    }
+    
+    public void setNumber(int number) {
+        Number = number;
+    }
+    
+    public List<Node> getChildren() {
+        return Children;
+    }
+    
+    public Node[] getChildrenAsArray() {
+        return (Node[]) Children.toArray();
+    }
+    
+    public Node getLastChild() {
+        if (Children.isEmpty()){
+            return null;
+        }
+        else {
+            return Children.get(Children.size()-1);
+        }
+    }
+    
+    public Node getFirstchild() {
+        if (Children.isEmpty()){
+            return null;
+        }
+        else {
+            return Children.get(Children.size()-1);
+        }
+    }
+    
+    public String getValue() {
+        return Value;
+    }
+    
+    public Node getParent() {
+        return Parent;
+    }
+    
+    public int getNumberOfChildren() {
+        return Children.size();
+    }
+    
+    public boolean hasChildren() {
+        return !Children.isEmpty();
+    }
+        
+    
+    public void addChild(Node node) {
+        if(node != this) {
+            node.setParent(this);
+            Children.add(node);
+        } else {
+            System.out.println("Eror! Cannot make child the node itself");
+        }
+    }
+    
+    public void addChild(String data) {
+        Node child = new Node(data, this);
+        Children.add(child);
+    }
+    
+    public void addChildren(List<Node> children) {
+       for(Node child : children){
+           if(child != this){
+               addChild(child);
+           }
+       }
+    }
+    
+    public void addChildren(String[] values) {
+        for (String value : values) {
+           addChild(value);
+        }
+    }
+    
+}
---
+++ TreeSimilarity/src/main/java/com/mycompany/treesimilarity/Tree.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.treesimilarity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Towny
+ */
+
+/*
+TODO: Set way to control and manipulate node numbers?
+*/
+public class Tree {
+    private Node Root;
+    int Index;
+    
+    Tree(Node root) {
+        Root = root;
+    }
+    
+    //get Last Node recursively
+    public Node getLastNode() {
+        Node node = Root;
+        while(node.getLastChild() != null){
+            node = node.getLastChild();
+        }
+        return node;
+    }
+    
+    public Node getRoot() {
+        return Root;
+    }
+    
+    
+    //recursive functions to get size of tree
+    //requires testing
+    public int getSize() {
+        //num is size 1 for root
+        int num = 1;
+        //call size on root
+        return size(Root, num);
+    }
+    
+    private int size(Node node, int num) {
+        //get number of children
+        num = num + node.getNumberOfChildren();
+        //for each child, recursively call self
+        for (Node child : node.getChildren()) {
+            size(child, num);
+        }
+        return num;
+    }
+    
+    public void addChildToLastNode(Node child) {
+        Node node = getLastNode();
+        node.addChild(child);
+    }
+    
+    public void addChildToRoot(Node child) {
+        Root.addChild(child);
+    }
+    
+    //TODO: Finish 
+    /*public List<Node> getListOfLeafNodes(){
+        List<Node> leafNodes = new ArrayList<Node>();
+        Node node = Root;
+        
+        while(node.hasChildren()) {
+            
+        }
+        
+        return leafNodes;
+    }
+    */
+    
+    /*
+    TODO: List of parent nodes??
+    TODO: Get Node by Number
+    */
+    public static void main(String[] args) {
+        Tree tree = new Tree(new Node("Hello there"));
+    }
+}
